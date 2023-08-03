0ab1bbb99bf5be18c142543311cb1efd7b33705d
KritPaoIn <kritpaointara@gmail.com>
2023-08-03 16:42:19 +0700

    refactor: rename project

--- src/components/ComponentContainer.tsx
+++ src/components/ComponentContainer.tsx
@@ -1,19 +1,17 @@
 interface ComponentContainerProps {
   name: string;
-  day: number;
   children: React.ReactNode;
 }
 
 const ComponentContainer: React.FC<ComponentContainerProps> = ({
   name,
-  day,
   children,
 }) => {
   return (
     <section className="bg-primary border-primary border-b">
       <div className="bg-primary mx-auto max-w-3xl pb-20 md:px-6">
         <h2 className="text-secondary p-6 text-center md:text-left">
-          Day {day} - {name}
+          {name}
         </h2>
         <div className="border-primary bg-secondary flex items-center justify-center border-y py-12 shadow-inner md:rounded-3xl md:border-x">
           {children}
